df765677050161ea6612c26590c0b658594d05ec
Pratyush <74895862+pratyushnishchal007@users.noreply.github.com>
2023-02-26 21:01:48 +0530

    Add files via upload

---
+++ userLogin.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="userLogin.aspx.cs" Inherits="Library_Management_System.userLogin" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mx-auto">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <center>
+                                    <img width="150" src="imgs/generaluser.png" />
+                                </center>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <center>
+                                    <h3>Member Login</h3>
+                                </center>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <hr>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <label>Member ID</label>
+                                <div class="form-group">
+                                    <asp:TextBox CssClass="form-control mt-2 mb-1" ID="TextBox1" runat="server" placeholder="Member ID"></asp:TextBox>
+                                </div>
+                                <label>Password</label>
+                                <div class="form-group">
+                                    <asp:TextBox CssClass="form-control mt-2 mb-1" ID="TextBox2" runat="server" placeholder="Password" TextMode="Password"></asp:TextBox>
+                                </div>
+                                <div class="form-group">
+                                    <asp:Button class="btn btn-success w-100 btn-lg mt-2 mb-1" ID="Button1" runat="server" Text="Login" OnClick="Button1_Click" />
+                                </div>
+                                <div class="form-group">
+                                    <a href="usersignup.aspx">
+                                        <input class="btn btn-info w-100 btn-lg mt-2" id="Button2" type="button" value="Sign Up" /></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a href="homepage.aspx"><< Back to Home</a><br>
+                <br>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ userProfile.aspx.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Library_Management_System
+{
+    public partial class userProfile : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString; 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Session["username"] == null || Session["username"].ToString()== "")
+                {
+                    Response.Write("<script>alert('Session expired Login again')</script>");
+                    Response.Redirect("userLogin.aspx");
+                }
+                else
+                {
+                    getUsersBookDetails();
+                    if(!Page.IsPostBack)
+                    {
+                        getUserPersonalDetails();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+        // update button
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Session["username"] == null || Session["username"].ToString() == "")
+                {
+                    Response.Write("<script>alert('Session expired Login again')</script>");
+                    Response.Redirect("userLogin.aspx");
+                }
+                else
+                {
+                    updateUserPersonalDetails();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+        // user defined function
+
+        void updateUserPersonalDetails()
+        {
+            string password = "";
+            if (TextBox10.Text.Trim() == "")
+            {
+                password = TextBox9.Text.Trim();
+            }
+            else
+            {
+                password = TextBox10.Text.Trim();
+            }
+            try
+            {
+                SqlConnection con=new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("UPDATE member_master_tbl SET full_name=@full_name,dob=@dob,contact_no=@contact_no,email=@email,state=@state,city=@city,pincode=@pincode,full_address=@full_address,password=@password,account_status=@account_status WHERE member_id='" + Session["username"].ToString() +"';", con);
+                cmd.Parameters.AddWithValue("@full_name", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@dob", TextBox2.Text.Trim());
+                cmd.Parameters.AddWithValue("@contact_no", TextBox3.Text.Trim());
+                cmd.Parameters.AddWithValue("@email", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@state", DropDownList1.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@city", TextBox6.Text.Trim());
+                cmd.Parameters.AddWithValue("@pincode", TextBox7.Text.Trim());
+                cmd.Parameters.AddWithValue("@full_address", TextBox5.Text.Trim());
+                cmd.Parameters.AddWithValue("@password",password);
+                cmd.Parameters.AddWithValue("@account_status", "pending");
+                int result=cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    Response.Write("<script>alert('Your Details are updated successfully');<script>");
+                    getUserPersonalDetails();
+                    getUsersBookDetails();
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid Entry');<script>");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+        void getUserPersonalDetails()
+        {
+            try
+            {
+                SqlConnection con=new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("SELECT * FROM member_master_tbl WHERE member_id='" + Session["username"].ToString() +"';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                TextBox1.Text = dt.Rows[0][0].ToString().Trim();
+                TextBox2.Text = dt.Rows[0][1].ToString().Trim();
+                TextBox3.Text = dt.Rows[0][2].ToString().Trim();
+                TextBox4.Text = dt.Rows[0][3].ToString().Trim();
+                DropDownList1.SelectedValue = dt.Rows[0][4].ToString().Trim();
+                TextBox6.Text = dt.Rows[0][5].ToString().Trim();
+                TextBox7.Text = dt.Rows[0][6].ToString().Trim();
+                TextBox5.Text = dt.Rows[0][7].ToString().Trim();
+                TextBox8.Text = dt.Rows[0][8].ToString().Trim();
+                TextBox9.Text = dt.Rows[0][9].ToString().Trim();
+                Label1.Text = dt.Rows[0][10].ToString().Trim();
+                if (dt.Rows[0][10].ToString().Trim() == "active")
+                {
+                    Label1.Attributes.Add("class", "badge rounded-pill bg-success text-white");
+                }
+                else if (dt.Rows[0][10].ToString().Trim()=="pending")
+                {
+                    Label1.Attributes.Add("class", "badge rounded-pill bg-warning text-white");
+                }
+                else if (dt.Rows[0][10].ToString().Trim()== "deactive")
+                {
+                    Label1.Attributes.Add("class", "badge rounded-pill bg-danger text-white");
+                }
+                else
+                {
+                    Label1.Attributes.Add("class", "badge rounded-pill bg-info text-dark");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+        void getUsersBookDetails()
+        {
+            try
+            {
+                SqlConnection con=new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("SELECT * FROM book_issue_table WHERE member_id='" + Session["username"].ToString() +"';", con);
+                SqlDataAdapter da=new SqlDataAdapter(cmd);
+                DataTable dt=new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource= dt;
+                GridView1.DataBind();
+
+
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            try
+            {
+                if (e.Row.RowType == DataControlRowType.DataRow)
+                {
+                    //Check your condition here
+                    DateTime dt = Convert.ToDateTime(e.Row.Cells[5].Text);
+                    DateTime today = DateTime.Now;
+                    if (today > dt)
+                    {
+                        e.Row.BackColor = System.Drawing.Color.PaleVioletRed;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        
+    }
+}
